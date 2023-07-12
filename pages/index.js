ec44b370d0831b8f3be1d2970da8efd8c56f1847
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-07-12 16:11:29 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div>
       <h1>Hello, World!</h1>
+      <Link href="/upload">
+        <a>Upload an Image</a>
+      </Link>
     </div>
   )
 }
+
