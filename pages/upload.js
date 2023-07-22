59487e90d0ff0d369f190020ced119618c1522bc
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-07-22 15:48:47 +0300

    Update upload.js

--- pages/upload.js
+++ pages/upload.js
@@ -4,6 +4,7 @@ export default function UploadPage() {
   const [selectedFile, setSelectedFile] = useState();
   const [originalImageUrl, setOriginalImageUrl] = useState();
   const [overlayImageUrl, setOverlayImageUrl] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   const [clicks, setClicks] = useState([]);
   const imageRef = useRef(null);
 
@@ -13,15 +14,10 @@ export default function UploadPage() {
         const rect = imageRef.current.getBoundingClientRect();
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;
-
-        // Determine if the click is positive or negative
-        var is_positive = true;  // Update this based on your application's logic
-
+        var is_positive = true;
         setClicks((oldClicks) => [...oldClicks, {x, y, is_positive}]);
       });
     }
-
-    // Don't forget to clean up the event listener when the component unmounts
     return () => {
       if (imageRef.current) {
         imageRef.current.removeEventListener('click');
@@ -31,6 +27,7 @@ export default function UploadPage() {
 
   const submitHandler = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
     const formData = new FormData();
     formData.append('file', selectedFile);
     formData.append('clicks', JSON.stringify(clicks));
@@ -48,6 +45,7 @@ export default function UploadPage() {
     } else {
       console.error('Upload failed.');
     }
+    setIsLoading(false);
   };
 
   const fileChangedHandler = (event) => {
@@ -61,8 +59,14 @@ export default function UploadPage() {
         <input type="file" onChange={fileChangedHandler} />
         <button type="submit">Upload</button>
       </form>
-      {originalImageUrl && <img ref={imageRef} src={originalImageUrl} alt="Original" width="400" height="400" />}
-      {overlayImageUrl && <img src={overlayImageUrl} alt="Overlay" width="400" height="400" />}
+      <div style={{ position: 'relative', width: 400, height: 400, border: '1px solid black' }}>
+        {isLoading && <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.5)' }}>Loading...</div>}
+        {originalImageUrl && <img ref={imageRef} src={originalImageUrl} alt="Original" width="400" height="400" />}
+      </div>
+      <div style={{ position: 'relative', width: 400, height: 400, border: '1px solid black' }}>
+        {isLoading && <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.5)' }}>Loading...</div>}
+        {overlayImageUrl && <img src={overlayImageUrl} alt="Overlay" width="400" height="400" />}
+      </div>
     </div>
   );
 }
