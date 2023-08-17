28ce378390cdc230f89ca625d50cdfc1d99f1400
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-18 02:57:13 +0300

    Update ImageUploader.js

--- components/ImageUploader.js
+++ components/ImageUploader.js
@@ -1,75 +1,49 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useCallback } from 'react';
 import ReactCrop from 'react-image-crop';
+import { useDropzone } from 'react-dropzone';
 import 'react-image-crop/dist/ReactCrop.css';
 import { Button, Stack } from '@mui/material';
-import { useDropzone } from 'react-dropzone';
 
 export default function ImageUploader({ onUpload }) {
-  const [selectedFile, setSelectedFile] = useState();
   const [originalImageUrl, setOriginalImageUrl] = useState();
   const [crop, setCrop] = useState({ aspect: 1 / 1 });
   const [completedCrop, setCompletedCrop] = useState(null);
-  const imgRef = useRef(null);
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    accept: 'image/*',
-    onDrop: (acceptedFiles) => {
-      setSelectedFile(acceptedFiles[0]);
-    },
-  });
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = acceptedFiles[0];
+    const reader = new FileReader();
 
-  const submitHandler = async (event) => {
-    event.preventDefault();
-    if (!selectedFile) return;
+    reader.onloadend = () => {
+      setOriginalImageUrl(reader.result);
+    };
 
-    const formData = new FormData();
-    formData.append('file', selectedFile);
-
-    const response = await fetch('https://www.sunsolve.co/uploadfile/', {
-      method: 'POST',
-      body: formData,
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      setOriginalImageUrl(data.original_image_url);
-      onUpload(selectedFile.name, data.original_image_url);
-    } else {
-      console.error('Upload failed.');
+    if (file) {
+      reader.readAsDataURL(file);
+      onUpload(file.name, reader.result);
     }
-  };
+  }, [onUpload]);
 
-  const onLoad = (img) => {
-    imgRef.current = img;
-  };
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
     <div>
       <Stack direction="row" spacing={2} alignItems="center">
-        <div {...getRootProps()}>
+        <div {...getRootProps()} style={{ border: '2px dashed #000', padding: '10px', cursor: 'pointer' }}>
           <input {...getInputProps()} />
-          {
-            isDragActive ?
-              <Button variant="contained">Drop the image here</Button> :
-              <Button variant="contained" component="span">Upload</Button>
-          }
+          <p>Drag and drop an image here, or click to select an image</p>
         </div>
-        <Button variant="contained" onClick={submitHandler}>
-          Submit
-        </Button>
+        {originalImageUrl && (
+          <div>
+            <ReactCrop
+              src={originalImageUrl}
+              crop={crop}
+              onChange={c => setCrop(c)}
+              onComplete={c => setCompletedCrop(c)}
+              style={{ maxWidth: '400px', maxHeight: '400px' }}
+            />
+          </div>
+        )}
       </Stack>
-      {originalImageUrl && (
-        <div>
-          <ReactCrop
-            src={originalImageUrl}
-            onImageLoaded={onLoad}
-            crop={crop}
-            onChange={c => setCrop(c)}
-            onComplete={c => setCompletedCrop(c)}
-            style={{ maxWidth: '400px', maxHeight: '400px' }}
-          />
-        </div>
-      )}
     </div>
   );
 }
