63d93086107c3c543b69d33e13f05afdc20b6d6e
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-19 00:35:04 +0300

    Update upload.js

--- pages/upload.js
+++ pages/upload.js
@@ -1,20 +1,42 @@
 import React, { useState } from 'react';
 import ImageSegmentor from '../components/ImageSegmentor.js';
 import { Button } from '@mui/material';
-
+import { Line } from 'react-chartjs-2'; // Don't forget to import the Line chart
 
 export default function UploadPage() {
-  const [imageSegmentors, setImageSegmentors] = useState([0]); // State to hold the count of ImageSegmentor components
-  const [segmentationComplete, setSegmentationComplete] = useState(false); // Flag to check if segmentation is complete
+  const [imageSegmentors, setImageSegmentors] = useState([0]); 
+  const [segmentationComplete, setSegmentationComplete] = useState(false);
+  // State to hold the plot data
+  const [plotData, setPlotData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: 'Delta E Values',
+        data: [],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1,
+      },
+    ],
+  });
 
   // Handler to add a new ImageSegmentor component
   const addImageSegmentor = () => {
     setImageSegmentors([...imageSegmentors, imageSegmentors.length]);
-    setSegmentationComplete(false); // Reset the segmentation complete flag
+    setSegmentationComplete(false);
   };
 
-  // Callback to mark segmentation as complete
-  const handleSegmentationComplete = () => {
+  // Callback to mark segmentation as complete and update plot data
+  const handleSegmentationComplete = (deltaE, filename) => {
+    setPlotData(prevData => ({
+      labels: [...prevData.labels, `Segment ${filename}`],
+      datasets: [
+        {
+          ...prevData.datasets[0],
+          data: [...prevData.datasets[0].data, deltaE],
+        },
+      ],
+    }));
     setSegmentationComplete(true);
   };
 
@@ -24,20 +46,23 @@ export default function UploadPage() {
       <div style={{ display: 'flex' }}>
         {imageSegmentors.map((segmentor, index) => (
           <div key={index} style={{ marginRight: '16px' }}>
-            <ImageSegmentor onSegmentationComplete={handleSegmentationComplete} />
+            <ImageSegmentor onSegmentationComplete={(deltaE, filename) => handleSegmentationComplete(deltaE, filename)} />
           </div>
         ))}
         {segmentationComplete && (
           <Button
             variant="contained"
             color="primary"
-            style={{ height: '400px', width: '400px' }} // Match the height and width of the image
+            style={{ height: '400px', width: '400px' }} 
             onClick={addImageSegmentor}
           >
             +
           </Button>
         )}
-      </div> {/* This closing div tag was missing */}
+      </div>
+      <div>
+        <Line data={plotData} /> {/* Render the plot below the images */}
+      </div>
     </div>
   );
 }
