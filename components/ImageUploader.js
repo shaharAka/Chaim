f745829eb5213d06cb73d7f28f92bd59e4434685
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-18 16:20:48 +0300

    Update ImageUploader.js

--- components/ImageUploader.js
+++ components/ImageUploader.js
@@ -4,10 +4,9 @@ import { useDropzone } from 'react-dropzone';
 import 'react-image-crop/dist/ReactCrop.css';
 import { Button, Stack } from '@mui/material';
 
-export default function ImageUploader({ onUpload, setCompletedCrop, onImageLoaded }) {
+export default function ImageUploader({ onUpload, setCompletedCrop }) {
   const [originalImageUrl, setOriginalImageUrl] = useState();
   const [crop, setCrop] = useState({ aspect: 1 / 1 });
-  const [completedCrop, setCompletedCrop] = useState(null);
 
   const onDrop = useCallback((acceptedFiles) => {
     const file = acceptedFiles[0];
@@ -28,14 +27,19 @@ export default function ImageUploader({ onUpload, setCompletedCrop, onImageLoade
   return (
     <div>
       <Stack direction="row" spacing={2} alignItems="center">
+        {!originalImageUrl && (
+          <div {...getRootProps()} style={{ border: '2px dashed #000', padding: '10px', cursor: 'pointer' }}>
+            <input {...getInputProps()} />
+            <p>Drag and drop an image here, or click to select an image</p>
+          </div>
+        )}
         {originalImageUrl && (
           <div>
             <ReactCrop
               src={originalImageUrl}
-              onImageLoaded={onImageLoaded} // Added this line
               crop={crop}
               onChange={c => setCrop(c)}
-              onComplete={c => setCompletedCrop(c)} // Modified this line
+              onComplete={c => setCompletedCrop(c)}
               style={{ maxWidth: '400px', maxHeight: '400px' }}
             />
           </div>
