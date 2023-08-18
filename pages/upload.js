ce1f183b967f9ecbf1c5991cc0d85a56fcffb0f9
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-18 23:15:31 +0300

    Update upload.js

--- pages/upload.js
+++ pages/upload.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ImageSegmentor from '../components/ImageSegmentor.js'; 
+import React, { useState } from 'react';
+import ImageSegmentor from '../components/ImageSegmentor.js';
 
 export default function UploadPage() {
+  const [imageSegmentors, setImageSegmentors] = useState([<ImageSegmentor key={0} />]);
+
+  const addImageSegmentor = () => {
+    setImageSegmentors([
+      ...imageSegmentors,
+      <ImageSegmentor key={imageSegmentors.length} />,
+    ]);
+  };
+
   return (
     <div>
       <h1>Upload Image</h1>
-      <ImageSegmentor /> 
-      <ImageSegmentor />
+      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+        {imageSegmentors}
+      </div>
+      <button onClick={addImageSegmentor} style={{ marginTop: '20px' }}>+</button>
     </div>
   );
 }
+
