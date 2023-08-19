92121219ee620ffca7f36038f4855eff5a551e1c
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-19 21:59:04 +0300

    Update ImageUploader.js

--- components/ImageUploader.js
+++ components/ImageUploader.js
@@ -4,27 +4,27 @@ import { useDropzone } from 'react-dropzone';
 import 'react-image-crop/dist/ReactCrop.css';
 import { Stack, Button } from '@mui/material';
 
-export default function ImageUploader({ onUpload, setCompletedCrop }) {
+export default function ImageUploader({ onUpload, setCompletedCrop, onImageLoaded }) {
   const [originalImageUrl, setOriginalImageUrl] = useState();
   const [crop, setCrop] = useState({ aspect: 1 / 1 });
 
   const onDrop = useCallback((acceptedFiles) => {
-  const file = acceptedFiles[0];
-  const reader = new FileReader();
+    const file = acceptedFiles[0];
+    const reader = new FileReader();
 
-  reader.onloadend = () => {
-    const result = reader.result;
-    setOriginalImageUrl(result);
+    reader.onloadend = () => {
+      const result = reader.result;
+      setOriginalImageUrl(result);
+      if (file) {
+        console.log('Calling onUpload:', file.name, result);
+        onUpload(file.name, result); // Call onUpload here after reading is complete
+      }
+    };
+
     if (file) {
-      console.log('Calling onUpload:', file.name, result);
-      onUpload(file.name, result); // Call onUpload here after reading is complete
+      reader.readAsDataURL(file);
     }
-  };
-
-  if (file) {
-    reader.readAsDataURL(file);
-  }
-}, [onUpload]);
+  }, [onUpload]);
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
@@ -44,6 +44,7 @@ export default function ImageUploader({ onUpload, setCompletedCrop }) {
               crop={crop}
               onChange={c => setCrop(c)}
               onComplete={c => setCompletedCrop(c)}
+              onImageLoaded={onImageLoaded} // Use the onImageLoaded prop here
               style={{ maxWidth: '400px', maxHeight: '400px' }}
             />
           </div>
