460ff157654ab09bbd142c44db44dd204c75410e
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-19 20:23:06 +0300

    Update upload.js

--- pages/upload.js
+++ pages/upload.js
@@ -3,9 +3,13 @@ import ImageSegmentor from '../components/ImageSegmentor.js';
 import { Button } from '@mui/material';
 import { Line } from 'react-chartjs-2';
 
+// You can import simple-statistics to perform linear regression
+import simpleStats from 'simple-statistics';
+
 export default function UploadPage() {
   const [imageSegmentors, setImageSegmentors] = useState([0]); 
   const [segmentationComplete, setSegmentationComplete] = useState(false);
+
   const [plotData, setPlotData] = useState({
     labels: [],
     datasets: [
@@ -16,24 +20,51 @@ export default function UploadPage() {
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1,
       },
+      {
+        label: 'Constant Line at 100',
+        data: new Array(10).fill(100), // Assuming you have at most 10 data points
+        fill: false,
+        borderColor: 'red',
+        borderDash: [5, 5], // This creates a dashed line
+        tension: 0,
+      }
     ],
   });
 
+  // Handler to add a new ImageSegmentor component
   const addImageSegmentor = () => {
     setImageSegmentors([...imageSegmentors, imageSegmentors.length]);
     setSegmentationComplete(false);
   };
 
+  // Callback to mark segmentation as complete and update plot data
   const handleSegmentationComplete = (deltaE, filename) => {
-    setPlotData(prevData => ({
-      labels: [...prevData.labels, `Segment ${filename}`],
-      datasets: [
-        {
-          ...prevData.datasets[0],
-          data: [...prevData.datasets[0].data, deltaE],
-        },
-      ],
-    }));
+    setPlotData(prevData => {
+      const newLabels = [...prevData.labels, `Segment ${filename}`];
+      const newData = [...prevData.datasets[0].data, deltaE];
+
+      // Compute linear regression
+      const linearFit = simpleStats.linearRegression(newData.map((y, x) => [x, y]));
+      const linearData = newLabels.map((_, x) => linearFit.m * x + linearFit.b);
+
+      return {
+        labels: newLabels,
+        datasets: [
+          {
+            ...prevData.datasets[0],
+            data: newData,
+          },
+          prevData.datasets[1], // Keep the constant line
+          {
+            label: 'Linear Fit',
+            data: linearData,
+            fill: false,
+            borderColor: 'blue',
+            tension: 0.1,
+          },
+        ],
+      };
+    });
     setSegmentationComplete(true);
   };
 
