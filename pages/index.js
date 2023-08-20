54ebced382f9a5d156edb7bea660d18bcc92399a
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-21 02:03:16 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,102 +1,101 @@
 import React from 'react';
-import { Container, Drawer, List, ListItem, ListItemIcon, ListItemText, Box, Typography, TextField, Grid, Button } from '@mui/material';
-import PersonIcon from '@mui/icons-material/Person';
-import AnalysisIcon from '@mui/icons-material/BarChart';
-import DoctorIcon from '@mui/icons-material/LocalHospital';
+import { Container, AppBar, Tabs, Tab, Box, Typography, TextField, Grid, Button } from '@mui/material';
 import Link from 'next/link';
 
 export default function Home() {
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (newValue) => {
+  const handleChange = (event, newValue) => {
     setValue(newValue);
+    console.log(`Value changed: ${newValue}`);
+    console.log(`Current value: ${value}`);
+    switch (newValue) {
+      case 0:
+        console.log('Rendering Patient Details');
+        break;
+      case 1:
+        console.log('Rendering Analysis');
+        break;
+      case 2:
+        console.log('Rendering Doctor Summary');
+        break;
+      default:
+        break;
+    }
   };
 
   return (
     <Container>
-      <Drawer
-        variant="permanent"
-        open
-        sx={{
-          '& .MuiDrawer-paper': {
-            backgroundColor: 'grey',
-            color: 'white',
-            width: 240
-          }
-        }}
-      >
-        <List>
-          <ListItem button onClick={() => handleChange(0)}>
-            <ListItemIcon><PersonIcon /></ListItemIcon>
-            <ListItemText primary="Patient Details" />
-          </ListItem>
-          <ListItem button onClick={() => handleChange(1)}>
-            <ListItemIcon><AnalysisIcon /></ListItemIcon>
-            <ListItemText primary="Analysis" />
-          </ListItem>
-          <ListItem button onClick={() => handleChange(2)}>
-            <ListItemIcon><DoctorIcon /></ListItemIcon>
-            <ListItemText primary="Doctor Summary" />
-          </ListItem>
-        </List>
-      </Drawer>
-      <Box sx={{ marginLeft: 240, width: 'calc(100% - 240px)' }}>
-        {value === 0 && (
-          <div>
-            <Typography variant="h5" gutterBottom>
-              Patient Details
-            </Typography>
-            <form>
-              <Grid container spacing={3}>
-                <Grid item xs={12} sm={6}>
-                  <TextField label="Name" variant="outlined" fullWidth />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField label="ID" variant="outlined" fullWidth />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField label="Age" variant="outlined" fullWidth type="number" />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    label="Sex"
-                    variant="outlined"
-                    select
-                    fullWidth
-                    SelectProps={{
-                      native: true,
-                    }}
-                  >
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                  </TextField>
-                </Grid>
-              </Grid>
-            </form>
-          </div>
-        )}
-        {value === 1 && (
-          <div>
-            <Link href="/upload">
-              <a>Upload an Image</a>
-            </Link>
-          </div>
-        )}
-        {value === 2 && (
-          <div>
-            <TextField
-              label="Doctor's Summary"
-              multiline
-              rows={4}
-              variant="outlined"
-              fullWidth
-            />
-            <Button variant="contained" color="primary" disabled>
-              Share Report
-            </Button>
-          </div>
-        )}
-      </Box>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Patient Details" />
+          <Tab label="Analysis" />
+          <Tab label="Doctor Summary" />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <Typography variant="h5" gutterBottom>
+          Patient Details
+        </Typography>
+        <form>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <TextField label="Name" variant="outlined" fullWidth />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField label="ID" variant="outlined" fullWidth />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField label="Age" variant="outlined" fullWidth type="number" />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Sex"
+                variant="outlined"
+                select
+                fullWidth
+                SelectProps={{
+                  native: true,
+                }}
+              >
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </TextField>
+            </Grid>
+          </Grid>
+        </form>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Link href="/upload">
+          <a>Upload an Image</a>
+        </Link>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <TextField
+          label="Doctor's Summary"
+          multiline
+          rows={4}
+          variant="outlined"
+          fullWidth
+        />
+        <Button variant="contained" color="primary" disabled>
+          Share Report
+        </Button>
+      </TabPanel>
     </Container>
+  );
+}
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div role="tabpanel" hidden={value !== index} {...other}>
+      {value === index && (
+        <Box>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
   );
 }
