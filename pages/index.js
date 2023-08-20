ced76b1e3d55e3ebacb15b063fcb2136c3c48508
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-21 02:22:43 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,50 @@
 import React from 'react';
-import { Container, AppBar, Tabs, Tab, Box, Typography, TextField, Grid, Button } from '@mui/material';
-import Link from 'next/link';
+import { Container, Drawer, List, ListItem, ListItemText, Box, Typography, TextField, Grid, Button } from '@mui/material';
 
 export default function Home() {
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
+  const handleChange = (newValue) => {
     setValue(newValue);
-    console.log(`Value changed: ${newValue}`);
-    console.log(`Current value: ${value}`);
-    switch (newValue) {
-      case 0:
-        console.log('Rendering Patient Details');
-        break;
-      case 1:
-        console.log('Rendering Analysis');
-        break;
-      case 2:
-        console.log('Rendering Doctor Summary');
-        break;
-      default:
-        break;
-    }
   };
 
   return (
-    <Container>
-      <AppBar position="static">
-        <Tabs value={value} onChange={handleChange}>
-          <Tab label="Patient Details" />
-          <Tab label="Analysis" />
-          <Tab label="Doctor Summary" />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        <Typography variant="h5" gutterBottom>
-          Patient Details
-        </Typography>
-        <form>
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={6}>
-              <TextField label="Name" variant="outlined" fullWidth />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField label="ID" variant="outlined" fullWidth />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField label="Age" variant="outlined" fullWidth type="number" />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                label="Sex"
-                variant="outlined"
-                select
-                fullWidth
-                SelectProps={{
-                  native: true,
-                }}
-              >
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-              </TextField>
-            </Grid>
-          </Grid>
-        </form>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <Link href="/upload">
-          <a>Upload an Image</a>
-        </Link>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <TextField
-          label="Doctor's Summary"
-          multiline
-          rows={4}
-          variant="outlined"
-          fullWidth
-        />
-        <Button variant="contained" color="primary" disabled>
-          Share Report
-        </Button>
-      </TabPanel>
-    </Container>
+    <Box sx={{ display: 'flex' }}>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: 240,
+            boxSizing: 'border-box',
+          },
+        }}
+      >
+        <List>
+          <ListItem button onClick={() => handleChange(0)}>
+            <ListItemText primary="Patient Details" />
+          </ListItem>
+          <ListItem button onClick={() => handleChange(1)}>
+            <ListItemText primary="Analysis" />
+          </ListItem>
+          <ListItem button onClick={() => handleChange(2)}>
+            <ListItemText primary="Doctor Summary" />
+          </ListItem>
+        </List>
+      </Drawer>
+      <Container sx={{ ml: 240 }}>
+        <TabPanel value={value} index={0}>
+          {/* Patient Details content here */}
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          {/* Analysis content here */}
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          {/* Doctor Summary content here */}
+        </TabPanel>
+      </Container>
+    </Box>
   );
 }
 
