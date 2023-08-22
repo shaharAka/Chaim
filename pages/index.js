88a2e27041e865dd93bb67b55dd65578f1cf5926
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-23 01:05:46 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import Link from 'next/link';
+import LeftMenu from '../components/leftMenu';
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the Medical Analysis Portal</h1>
-      <ul>
-        <li>
-          <Link href="/patientDetails" passHref>
-            <a>Patient Details</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/upload" passHref>
-            <a>Analysis</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/doctorSummary" passHref>
-            <a>Doctor Summary</a>
-          </Link>
-        </li>
-      </ul>
+      <LeftMenu /> 
     </div>
   );
 }
