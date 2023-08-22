ee4ba29e0e0a4fa6ea7807230bae094b00af275e
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-23 01:48:49 +0300

    Update ImageSegmentor.js

--- components/ImageSegmentor.js
+++ components/ImageSegmentor.js
@@ -1,28 +1,28 @@
 import ImageUploader from './ImageUploader.js';
-import { Stack, Button } from '@mui/material';
+import { Stack, Button, CircularProgress } from '@mui/material'; // Import CircularProgress for the spinner
 import { Line } from 'react-chartjs-2';
 import { TextField } from '@mui/material';
 import React, { useState, useRef, useCallback } from 'react';
-
 
 export default function ImageSegmenter({ onSegmentationComplete }) {
   const [originalImageUrl, setOriginalImageUrl] = useState();
   const [filename, setFilename] = useState();
   const [overlayImageUrl, setOverlayImageUrl] = useState();
   const [completedCrop, setCompletedCrop] = useState(null);
-  const [maskArea, setMaskArea] = useState(); // State to hold the mask area
-  const [deltaEValue, setDeltaEValue] = useState(); // State to hold the Delta E value
-  const [segmentationComplete, setSegmentationComplete] = useState(false); 
-  const [treatmentNumber, setTreatmentNumber] = useState('');
+  const [maskArea, setMaskArea] = useState();
+  const [deltaEValue, setDeltaEValue] = useState();
+  const [segmentationComplete, setSegmentationComplete] = useState(false);
+  const [isSegmenting, setIsSegmenting] = useState(false); // New state for managing when segmentation is happening
 
   const imgRef = useRef(null);
-  
+
   const onImageLoad = useCallback((img) => {
-  imgRef.current = img;
-  console.log('Image loaded:', imgRef.current); // Log here to make sure it's being set
-}, []);
+    imgRef.current = img;
+    console.log('Image loaded:', imgRef.current);
+  }, []);
 
   const segmentHandler = async () => {
+    setIsSegmenting(true); // Set isSegmenting to true when segmentation starts
     console.log('Segment handler called');
     console.log('completedCrop:', completedCrop);
     console.log('imgRef.current:', imgRef.current);
@@ -63,7 +63,7 @@ export default function ImageSegmenter({ onSegmentationComplete }) {
     // Log the scaled crop
     console.log('Scaled Crop:', scaledCrop);
 
-    const response = await fetch('https://www.sunsolve.co/segment/', {
+   const response = await fetch('https://www.sunsolve.co/segment/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -71,7 +71,7 @@ export default function ImageSegmenter({ onSegmentationComplete }) {
       body: JSON.stringify({
         filename: filename,
         image: originalImageUrl,
-        crop: JSON.stringify(scaledCrop), // Sending the scaled coordinates
+        crop: JSON.stringify(scaledCrop),
       }),
     });
 
@@ -79,62 +79,68 @@ export default function ImageSegmenter({ onSegmentationComplete }) {
       const data = await response.json();
       const maskBase64 = data.mask_base64;
       setOverlayImageUrl(`data:image/png;base64,${maskBase64}`);
-      setMaskArea(data.mask_area_mm2); // Set the mask area
+      setMaskArea(data.mask_area_mm2);
       setDeltaEValue(data.delta_e);
-     
+
       console.log('Segmented successfully!');
       if (onSegmentationComplete) {
-        onSegmentationComplete(data.delta_e, filename); 
+        onSegmentationComplete(data.delta_e, filename);
       }
       setSegmentationComplete(true);
+      setIsSegmenting(false); // Set isSegmenting to false when segmentation completes successfully
     } else {
       console.error('Segmentation failed.');
+      setIsSegmenting(false); // Set isSegmenting to false if segmentation fails
     }
   };
 
   return (
-  <div>
-    <ImageUploader
-      onUpload={(name, url) => {
-        console.log('Image uploaded:', name, url);
-        setFilename(name);
-        setOriginalImageUrl(url);
-      }}
-      setCompletedCrop={setCompletedCrop}
-      onImageLoaded={onImageLoad}
-    />
-    {originalImageUrl && (
-      <div>
-        {/* Input for Treatment Number */}
-        <TextField
-          label="Treatment Number"
-          variant="outlined"
-          value={treatmentNumber}
-          onChange={(e) => setTreatmentNumber(e.target.value)}
-          style={{ width: '400px', margin: '10px 0' }}
-        />
+    <div>
+      <ImageUploader
+        onUpload={(name, url) => {
+          console.log('Image uploaded:', name, url);
+          setFilename(name);
+          setOriginalImageUrl(url);
+        }}
+        setCompletedCrop={setCompletedCrop}
+        onImageLoaded={onImageLoad}
+      />
+      {originalImageUrl && (
+        <div>
+          {/* Input for Treatment Number */}
+          <TextField
+            label="Treatment Number"
+            variant="outlined"
+            value={treatmentNumber}
+            onChange={(e) => setTreatmentNumber(e.target.value)}
+            style={{ width: '400px', margin: '10px 0' }}
+          />
 
-        {!segmentationComplete && (
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ width: '400px' }} // Match the width of the image
-            onClick={segmentHandler}
-          >
-            Segment!
-          </Button>
-        )}
-        {overlayImageUrl && <img src={overlayImageUrl} alt="Overlay" style={{ width: "400px", height: "400px" }} />}
-        {maskArea !== undefined &&
-          <div className="info-box">
-            <div>Mask Area: {maskArea.toFixed(2)} mm<sup>2</sup></div>
-            <p>Delta E Value: {deltaEValue.toFixed(2)}</p>
-          </div>
-        }
-      </div>
-    )}
-  </div>
-);
+          {!segmentationComplete && (
+            isSegmenting ? (
+              <CircularProgress /> // Show spinner when segmenting
+            ) : (
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ width: '400px' }}
+                onClick={segmentHandler}
+              >
+                Segment!
+              </Button>
+            )
+          )}
+          {overlayImageUrl && <img src={overlayImageUrl} alt="Overlay" style={{ width: "400px", height: "400px" }} />}
+          {maskArea !== undefined &&
+            <div className="info-box">
+              <div>Mask Area: {maskArea.toFixed(2)} mm<sup>2</sup></div>
+              <p>Delta E Value: {deltaEValue.toFixed(2)}</p>
+            </div>
+          }
+        </div>
+      )}
+    </div>
+  );
 }
 
 
