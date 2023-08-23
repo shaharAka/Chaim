a0524f36faefad3d2f93d3694f121480196338f5
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-23 23:15:16 +0300

    Update ImageSegmentor.js

--- components/ImageSegmentor.js
+++ components/ImageSegmentor.js
@@ -74,7 +74,7 @@ export default function ImageSegmenter({ onSegmentationComplete, linearModel }) 
 
       console.log('Segmented successfully!');
       if (onSegmentationComplete) {
-        onSegmentationComplete(treatmentNumber, data.delta_e, filename); // Fix the order of parameters
+        onSegmentationComplete(treatmentNumber, data.delta_e, filename); 
       }
       setSegmentationComplete(true);
     } else {
