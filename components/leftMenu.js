c7ccaaf94905f7dce02db23a80b793f67221ba57
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-25 00:07:59 +0300

    Update leftMenu.js

--- components/leftMenu.js
+++ components/leftMenu.js
@@ -1,42 +1,59 @@
-import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import React, { useState } from 'react';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton, useMediaQuery } from '@mui/material';
 import PersonIcon from '@mui/icons-material/Person';
 import AnalysisIcon from '@mui/icons-material/BarChart';
 import DoctorIcon from '@mui/icons-material/LocalHospital';
+import MenuIcon from '@mui/icons-material/Menu';
 import Link from 'next/link';
 
 export default function LeftMenu() {
+  const [open, setOpen] = useState(false);
+  const isMobile = useMediaQuery('(max-width:600px)');
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
   return (
-    <Drawer
-      variant="permanent"
-      open
-      sx={{
-        '& .MuiDrawer-paper': {
-          backgroundColor: 'grey',
-          color: 'white',
-          width: 240,
-        },
-      }}
-    >
-      <List>
-        <Link href="/patientDetails" passHref>
-          <ListItem button component="a">
-            <ListItemIcon><PersonIcon /></ListItemIcon>
-            <ListItemText primary="Patient Details" />
-          </ListItem>
-        </Link>
-        <Link href="/upload" passHref>
-          <ListItem button component="a">
-            <ListItemIcon><AnalysisIcon /></ListItemIcon>
-            <ListItemText primary="Analysis" />
-          </ListItem>
-        </Link>
-        <Link href="/doctorSummary" passHref>
-          <ListItem button component="a">
-            <ListItemIcon><DoctorIcon /></ListItemIcon>
-            <ListItemText primary="Doctor Summary" />
-          </ListItem>
-        </Link>
-      </List>
-    </Drawer>
+    <div>
+      {isMobile && (
+        <IconButton onClick={toggleDrawer}>
+          <MenuIcon />
+        </IconButton>
+      )}
+      <Drawer
+        variant={isMobile ? 'temporary' : 'permanent'}
+        open={isMobile ? open : true}
+        onClose={toggleDrawer}
+        sx={{
+          '& .MuiDrawer-paper': {
+            backgroundColor: 'grey',
+            color: 'white',
+            width: 240,
+          },
+        }}
+      >
+        <List>
+          <Link href="/patientDetails" passHref>
+            <ListItem button component="a">
+              <ListItemIcon><PersonIcon /></ListItemIcon>
+              <ListItemText primary="Patient Details" />
+            </ListItem>
+          </Link>
+          <Link href="/upload" passHref>
+            <ListItem button component="a">
+              <ListItemIcon><AnalysisIcon /></ListItemIcon>
+              <ListItemText primary="Analysis" />
+            </ListItem>
+          </Link>
+          <Link href="/doctorSummary" passHref>
+            <ListItem button component="a">
+              <ListItemIcon><DoctorIcon /></ListItemIcon>
+              <ListItemText primary="Doctor Summary" />
+            </ListItem>
+          </Link>
+        </List>
+      </Drawer>
+    </div>
   );
 }
