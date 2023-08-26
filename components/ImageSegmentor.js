18829252422aea90adc3843c4a3ad914a60b86d9
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-26 22:35:44 +0300

    Update ImageSegmentor.js

--- components/ImageSegmentor.js
+++ components/ImageSegmentor.js
@@ -1,26 +1,20 @@
-import ImageUploader from './ImageUploader.js';
-import { Button, CircularProgress, TextField, useMediaQuery } from '@mui/material';
 import React, { useState, useRef, useCallback } from 'react';
+import { Button, CircularProgress, TextField, useMediaQuery } from '@mui/material';
+import ImageUploader from './ImageUploader.js';
 
-export default function ImageSegmenter({ onSegmentationComplete, linearModel }) {
-  const [originalImageUrl, setOriginalImageUrl] = useState();
-  const [filename, setFilename] = useState();
-  const [overlayImageUrl, setOverlayImageUrl] = useState();
-  const [completedCrop, setCompletedCrop] = useState(null);
-  const [maskArea, setMaskArea] = useState();
-  const [deltaEValue, setDeltaEValue] = useState();
-  const [segmentationComplete, setSegmentationComplete] = useState(false);
-  const [isSegmenting, setIsSegmenting] = useState(false);
+export default function ImageSegmenter({ onSegmentationComplete }) {
+  const [originalImageUrl, setOriginalImageUrl] = useState(null);
+  const [filename, setFilename] = useState(null);
   const [treatmentNumber, setTreatmentNumber] = useState('');
-  const imgRef = useRef(null);
-  const fileInputRef = useRef(null);
-    const isMobile = useMediaQuery('(max-width:600px)');
+  const [isSegmenting, setIsSegmenting] = useState(false);
+  const [deltaEValue, setDeltaEValue] = useState(null);
 
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const fileInputRef = useRef(null);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     if (file) {
-      // Read the file and pass it to ImageUploader
       const reader = new FileReader();
       reader.onloadend = () => {
         setFilename(file.name);
@@ -29,11 +23,6 @@ export default function ImageSegmenter({ onSegmentationComplete, linearModel }) 
       reader.readAsDataURL(file);
     }
   };
-
-  const onImageLoad = useCallback((img) => {
-    imgRef.current = img;
-    console.log('Image loaded:', imgRef.current);
-  }, []);
 
   const segmentHandler = async () => {
     setIsSegmenting(true);
@@ -110,59 +99,57 @@ export default function ImageSegmenter({ onSegmentationComplete, linearModel }) 
   const estimatedRemainingTreatments = estimateRemainingTreatments();
 
   return (
-  <div style={{ display: 'flex', flexDirection: 'row' }}>
     <div>
       {isMobile ? (
-        <input
-          type="file"
-          accept="image/*"
-          capture="camera"
-          ref={fileInputRef}
-          style={{ display: 'none' }}
-          onChange={handleFileChange}
-        />
+        <>
+          <input
+            type="file"
+            accept="image/*"
+            capture="camera"
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+            onChange={handleFileChange}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => fileInputRef.current.click()}
+          >
+            Capture or Select Image
+          </Button>
+        </>
       ) : (
         <ImageUploader
           onUpload={(name, url) => {
             setFilename(name);
             setOriginalImageUrl(url);
           }}
-          setCompletedCrop={setCompletedCrop}
-          onImageLoaded={onImageLoad}
         />
       )}
-      <Button
-        variant="contained"
-        color="primary"
-        style={{ width: '400px', display: 'block', margin: '10px 0' }}
-        onClick={() => fileInputRef.current.click()}
-      >
-        {isMobile ? 'Capture or Select Image' : 'Upload Image'}
-      </Button>
+
+      {originalImageUrl && (
+        <>
+          <img src={originalImageUrl} alt="Uploaded" style={{ width: '400px', height: '400px' }} />
+          <TextField
+            variant="outlined"
+            label="Treatment Number"
+            type="number"
+            value={treatmentNumber}
+            onChange={(e) => setTreatmentNumber(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={segmentHandler}
+          >
+            Segment!
+          </Button>
+        </>
+      )}
+
+      {isSegmenting && <CircularProgress />}
+      {deltaEValue !== undefined && <div>Delta E: {deltaEValue}</div>}
     </div>
-    {originalImageUrl && (
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <img src={originalImageUrl} alt="Original" style={{ width: '400px', height: '400px' }} />
-        <TextField
-          variant="outlined"
-          label="Treatment Number"
-          type="number"
-          value={treatmentNumber}
-          onChange={e => setTreatmentNumber(e.target.value)}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          style={{ margin: '10px 0' }}
-          onClick={segmentHandler}
-        >
-          Segment Image
-        </Button>
-        {isSegmenting && <CircularProgress />}
-        {deltaEValue !== undefined && <div>Delta E: {deltaEValue}</div>}
-      </div>
-    )}
-  </div>
-);
+  );
 }
 
