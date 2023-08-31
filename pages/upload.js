7fcb130f4d2e02b2ff8585c1742d6d6010de373b
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-08-31 23:41:16 +0300

    Update upload.js

--- pages/upload.js
+++ pages/upload.js
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react';
 import ReactCrop from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css';
 import LeftMenu from '../components/leftMenu';
-import { useMediaQuery, CircularProgress } from '@mui/material';
+import { useMediaQuery, CircularProgress, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 
 export default function UploadPage() {
   const [isUploading, setIsUploading] = useState(false);
@@ -112,43 +112,51 @@ export default function UploadPage() {
     }
   };
 
-  return (
+   return (
     <div style={mobileStyles.container}>
       <LeftMenu />
-      <h1 style={mobileStyles.title}>Upload Image</h1>
-      {!originalImageUrl && (
-        <form onSubmit={submitHandler}>
-          <input type="file" onChange={fileChangedHandler} style={mobileStyles.input} />
-          <button type="submit" style={mobileStyles.button}>
-            {isUploading ? <CircularProgress size={24} /> : 'Upload'}
-          </button>
-        </form>
-      )}
-      {originalImageUrl && (
-        <div>
-          <ReactCrop
-            src={originalImageUrl}
-            onImageLoaded={onLoad}
-            crop={crop}
-            onChange={(c) => setCrop(c)}
-            onComplete={(c) => setCompletedCrop(c)}
-            style={{ maxWidth: '400px', maxHeight: '400px' }}
-          />
-          <div style={mobileStyles.guideText}>Drag a segmentation area around the wound</div>
-          <button onClick={segmentHandler} style={mobileStyles.button}>
-            {isSegmenting ? <CircularProgress size={24} /> : 'Segment!'}
-          </button>
-        </div>
-      )}
-      {overlayImageUrl && <img src={overlayImageUrl} alt="Overlay" style={{ width: '400px', height: '400px' }} />}
-      <div>
-        {maskArea !== undefined && (
-          <div className="info-box">
-            <div>Mask Area: {maskArea.toFixed(2)} mm<sup>2</sup></div>
-            <p>Delta E Value: {deltaEValue.toFixed(2)}</p>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+            <h4>Treatment #1:</h4>
+            {deltaEValue !== undefined && <span>Delta E Value: {deltaEValue.toFixed(2)}</span>}
           </div>
-        )}
-      </div>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div style={{ width: '100%' }}>
+            <h1 style={mobileStyles.title}>Upload Image</h1>
+            {!originalImageUrl &&
+              <form onSubmit={submitHandler}>
+                <input type="file" onChange={fileChangedHandler} style={mobileStyles.input} />
+                <button type="submit" style={mobileStyles.button}>Upload</button>
+              </form>
+            }
+            {originalImageUrl && 
+              <div>
+                <ReactCrop
+                  src={originalImageUrl}
+                  onImageLoaded={onLoad}
+                  crop={crop}
+                  onChange={c => setCrop(c)}
+                  onComplete={c => setCompletedCrop(c)}
+                  style={{maxWidth: "400px", maxHeight: "400px"}}
+                />
+                <div style={mobileStyles.guideText}>Drag a segmentation area around the wound</div>
+                <button onClick={segmentHandler} style={mobileStyles.button}>
+                  {isSegmenting ? <CircularProgress size={24} /> : 'Segment!'}
+                </button>
+              </div>
+            }
+            {overlayImageUrl && <img src={overlayImageUrl} alt="Overlay" style={{width: "400px", height: "400px"}} />}
+            {maskArea !== undefined && 
+              <div className="info-box">
+                <div>Mask Area: {maskArea.toFixed(2)} mm<sup>2</sup></div>
+                <p>Delta E Value: {deltaEValue.toFixed(2)}</p> 
+              </div>
+            }
+          </div>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 }
