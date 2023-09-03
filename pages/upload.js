b8ecdce613fb6d3ed7fd316a0c907a82461bc4b6
shaharAka <138374017+shaharAka@users.noreply.github.com>
2023-09-03 23:51:12 +0300

    Update upload.js

--- pages/upload.js
+++ pages/upload.js
@@ -49,97 +49,57 @@ const TreatmentSection = ({ index, onSegmentDone }) => {
     }
   };
   const submitHandler = async (event) => {
-  event.preventDefault();
-  setIsUploading(true);
-
-  // Create a canvas element and get the 2D context
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
-
-  // Create a new image object
-  const img = new Image();
-  img.src = URL.createObjectURL(selectedFile);
-
-  img.onload = async () => {
-    // Set canvas dimensions to half of image dimensions
-    canvas.width = img.width / 2;
-    canvas.height = img.height / 2;
-
-    // Draw the image onto the canvas
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-    // Convert canvas to Blob
-    canvas.toBlob(async (blob) => {
-      const formData = new FormData();
-      formData.append('file', blob, selectedFile.name); // Send the scaled-down image
-
-      const response = await fetch('https://www.sunsolve.co/uploadfile/', {
-        method: 'POST',
-        body: formData,
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        setOriginalImageUrl(data.original_image_url);
-        setFilename(selectedFile.name);
-        setIsUploading(false);
-      } else {
-        console.error('Upload failed.');
-      }
+    event.preventDefault();
+    setIsUploading(true);
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+    const response = await fetch('https://www.sunsolve.co/uploadfile/', {
+      method: 'POST',
+      body: formData,
     });
+    if (response.ok) {
+      const data = await response.json();
+      setOriginalImageUrl(data.original_image_url);
+      setFilename(selectedFile.name);
+      setIsUploading(false);
+    } else {
+      console.error('Upload failed.');
+    }
   };
-};
   const segmentHandler = async () => {
-  setIsSegmenting(true);
-  
-  // Original scaling ratio
-  const scaleX = imgRef.current.naturalWidth / imgRef.current.width;
-  const scaleY = imgRef.current.naturalHeight / imgRef.current.height;
-
-  // Compute the original crop dimensions
-  const originalCrop = {
-    x: completedCrop.x * scaleX,
-    y: completedCrop.y * scaleY,
-    width: completedCrop.width * scaleX,
-    height: completedCrop.height * scaleY,
+    setIsSegmenting(true);
+    const scaleX = imgRef.current.naturalWidth / imgRef.current.width;
+    const scaleY = imgRef.current.naturalHeight / imgRef.current.height;
+    const scaledCrop = {
+      x: completedCrop.x * scaleX,
+      y: completedCrop.y * scaleY,
+      width: completedCrop.width * scaleX,
+      height: completedCrop.height * scaleY,
+      unit: completedCrop.unit,
+      aspect: completedCrop.aspect,
+    };
+    const response = await fetch('https://www.sunsolve.co/segment/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        filename: filename,
+        crop: JSON.stringify(scaledCrop),
+      }),
+    });
+    if (response.ok) {
+      const data = await response.json();
+      const maskBase64 = data.mask_base64;
+      setOverlayImageUrl(`data:image/png;base64,${maskBase64}`);
+      setMaskArea(data.mask_area_mm2);
+      setDeltaEValue(data.delta_e);
+      console.log("deltaEvalue 2", deltaEValue)
+      setIsSegmenting(false);
+    } else {
+      console.error('Segmentation failed.');
+    }
   };
-
-  // Scale down the crop dimensions by half
-  const scaledCrop = {
-    x: originalCrop.x / 2,
-    y: originalCrop.y / 2,
-    width: originalCrop.width / 2,
-    height: originalCrop.height / 2,
-    unit: completedCrop.unit,
-    aspect: completedCrop.aspect,
-  };
-
-  // Send the scaled crop dimensions to the server
-  const response = await fetch('https://www.sunsolve.co/segment/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      filename: filename,
-      crop: JSON.stringify(scaledCrop),
-    }),
-  });
-
-  if (response.ok) {
-    const data = await response.json();
-    const maskBase64 = data.mask_base64;
-    setOverlayImageUrl(`data:image/png;base64,${maskBase64}`);
-    setMaskArea(data.mask_area_mm2);
-    setDeltaEValue(data.delta_e);
-    console.log("deltaEvalue 2", deltaEValue);
-    setIsSegmenting(false);
-  } else {
-    console.error('Segmentation failed.');
-  }
-};
-
-
   const onLoad = (img) => {
     imgRef.current = img;
   };
